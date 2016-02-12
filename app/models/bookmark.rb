1676e177036781b7ccbc262801034d7ec2f9d36a
Luise Rauch <luise.rauch@gmail.com>
2016-02-12 14:13:52 +0100

    mailer not working

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,31 +1,19 @@
 class Bookmark < ActiveRecord::Base
+  # require 'uri'
+
   belongs_to :topic
   belongs_to :user
   has_many :likes, dependent: :destroy
 
   validates :topic, presence: true
   validates :user, presence: true, on: :create
-  validates :url, presence: true, format: { with: URI::regexp(%w(http https))}, uniqueness: { scope: :topic_id, message: '%{value} has already been saved to this topic.' }
+  validates :url, presence: true, uniqueness: { scope: :topic_id, message: '%{value} has already been saved to this topic.' }
+  validates :url, format: { with: URI::regexp(%w(http https))}
 
-  # after_destroy :reassign_liked_bookmarks
-
-  # asks users if they want to save a liked bookmark before its gone
-  # def reassign_liked_bookmarks
-  #   likes = self.likes
-    # check if the bookmarks's likes belong to any other user
-    # foreign_likes = likes.where.not(user_id: self.user_id)
-    #   if foreign_likes.any?
-    #     foreign_likes.each do |bookmark|
-      #ask if you want to save the bookmark as yours
-
-      #yes - save it
-      #like.user_id = foreign_likes.first.user_id
-      
-      #current_user = self.user
-      #self.save
-      #no - unlike it
-      #@like.destroy
-  #   end
+  # def valid?(url)
+  #   uri = URI.parse(url)
+  #   uri.kind_of?(URI::HTTP)
+  # rescue URI::InvalidURIError
+  #   false
   # end
-
 end
